b83fe2a15a37574f9352fad162bc8d39632f55e5
aaudelin <aurelien.audelin@gmail.com>
2021-07-22 11:44:37 +0200

    Init counter component

---
+++ src/counter/counter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './counter.css';
+
+function Counter() {
+  return (
+    <div>
+      <h2>Compteur : 0</h2>
+      <div>
+        <button>Incrémenter</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
